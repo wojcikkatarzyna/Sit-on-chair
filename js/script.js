73818d045e468de79460c9f48ed9053ab482ecab
Katarzyna Wojcik <wojcik.katarzyna.danuta@gmail.com>
2017-06-11 23:36:21 +0200

    add calculator without correct sum

--- js/script.js
+++ js/script.js
@@ -37,9 +37,6 @@ document.addEventListener('DOMContentLoaded', function(){
   var next = document.querySelector('.adArrowRight');
   var prev = document.querySelector('.adArrowLeft');
   var li = document.querySelectorAll('.slider > li');
-  console.log(next);
-  console.log(prev);
-  console.log(li);
 
   var index = 0;
   li[index].classList.remove('hidden');
@@ -63,6 +60,103 @@ document.addEventListener('DOMContentLoaded', function(){
     }
     li[index].classList.remove('hidden');
   });
+
+//calculator- choose chair
+
+  var arrows = document.querySelectorAll('.list_arrow');
+  console.log(arrows);
+  for (var i = 0; i < arrows.length; i++) {
+    arrows[i].addEventListener('click',function(){
+      this.nextElementSibling.classList.toggle('hidden');
+    });
+  }
+
+  //calculator- transport
+
+  var check = document.querySelector('.active');
+  var div = check.previousElementSibling
+
+  check.addEventListener('click', function(event){
+    event.preventDefault();
+    div.classList.add('agree');
+  });
+
+    div.addEventListener('click', function(event){
+      event.preventDefault();
+      this.classList.remove('agree');
+  });
+
+  //calculator-summary
+  var liChair = document.querySelectorAll(".chairs li");
+  var h4Left = document.querySelector(".panel_left h4");
+  var h4Right = document.querySelector(".panel_right h4");
+  var liColor = document.querySelectorAll(".color li");
+  var spanLeft = document.querySelectorAll(".panel_left span");
+  var spanRight = document.querySelectorAll(".panel_right span");
+  var liPattern = document.querySelectorAll(".pattern li");
+  var cost1 = 0;
+  var cost2 = 0;
+  var cost3 = 0;
+  var cost4 = 0;
+  var sum = document.querySelector(".sum");
+  var totalCost = 0;
+
+  for (var i = 0; i < liChair.length; i++) {
+    liChair[i].addEventListener('click',function(){
+      h4Left.innerText = this.innerText;
+      if (this.innerText === "Clair") {
+        h4Right.innerText = "200";
+      } else if (this.innerText === "Margarita") {
+        h4Right.innerText = "300";
+      } else {
+        h4Right.innerText = "400";
+      }
+      sum.innerText += parseInt(h4Right.innerText);
+    });
+
+  }
+
+  for (var i = 0; i < liColor.length; i++) {
+    liColor[i].addEventListener('click',function(){
+      spanLeft[0].innerText = this.innerText;
+      spanRight[0].innerText = "0";
+      sum.innerText += parseInt(spanRight[0].innerText);
+    });
+  }
+
+  for (var i = 0; i < liPattern.length; i++) {
+    liPattern[i].addEventListener('click',function(){
+      spanLeft[1].innerText = this.innerText;
+      if (this.innerText === "Tkanina") {
+        spanRight[1].innerText = "0";
+      } else {
+        spanRight[1].innerText = "100";
+      }
+      sum.innerText += parseInt(spanRight[1].innerText);
+    });
+  }
+
+  console.log(spanRight);
+  check.addEventListener('click', function(event){
+    spanLeft[2].innerText = "Transport";
+    spanRight[2].innerText = "200";
+    sum.innerText += parseInt(spanRight[2].innerText);
+  });
+
+
+
+
+
+  var rightPanel = document.querySelector(".panel_right");
+  var costs = rightPanel.children;
+  // for (var i = 0; i < costs.length; i++) {
+  //   totalCost += parseInt(costs[i]);
+  // }
+  // console.log(cost3);
+  // if (cost1 !== null) {
+  //   console.log(totalCost);
+
+  // }
 
 
 });
